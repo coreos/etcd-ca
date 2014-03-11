594437c6591bfcbfbe74758a93a44244d4bf564a
Yicheng Qin <qycqycqycqycqyc@gmail.com>
2014-03-11 12:56:25 -0700

    chore(depot): add comment for Tag

--- depot/depot.go
+++ depot/depot.go
@@ -7,6 +7,14 @@ import (
 	"path/filepath"
 )
 
+// Tag includes name and permission requirement
+// Permission requirement is used in two ways:
+// 1. Set the permission for data when Put
+// 2. Check the permission required when Get
+// It is set to prevent attacks from other users for FileDepot.
+// For example, 'evil' creates file ca.key with 0666 file perm,
+// 'core' reads it and uses it as ca.key. It may cause the security
+// problem of fake certificate and key.
 type Tag struct {
 	name string
 	// TODO(yichengq): make perm module take in charge later
