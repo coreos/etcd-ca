5768b3c09f3da8e19ac138854edf462f582775e9
Yicheng Qin <qycqycqycqycqyc@gmail.com>
2014-03-12 14:45:19 -0700

    fix(cmd): Move user interaction from Stdout to Stderr
    It avoids polluting stdout.

--- cmd/util.go
+++ cmd/util.go
@@ -4,9 +4,11 @@ import (
 	"bytes"
 	"errors"
 	"fmt"
+	"os"
 	"syscall"
 
 	"github.com/coreos/etcd-ca/third_party/code.google.com/p/go.crypto/ssh/terminal"
+	"github.com/coreos/etcd-ca/third_party/github.com/codegangsta/cli"
 
 	"github.com/coreos/etcd-ca/depot"
 )
@@ -26,17 +28,17 @@ func InitDepot(path string) error {
 }
 
 func createPassPhrase() ([]byte, error) {
-	fmt.Print("Enter passphrase (empty for no passphrase): ")
+	fmt.Fprint(os.Stderr, "Enter passphrase (empty for no passphrase): ")
 	pass1, err := terminal.ReadPassword(syscall.Stdin)
 	if err != nil {
 		return nil, err
 	}
-	fmt.Print("\nEnter same passphrase again: ")
+	fmt.Fprint(os.Stderr, "\nEnter same passphrase again: ")
 	pass2, err := terminal.ReadPassword(syscall.Stdin)
 	if err != nil {
 		return nil, err
 	}
-	fmt.Println()
+	fmt.Fprintln(os.Stderr)
 
 	if bytes.Compare(pass1, pass2) != 0 {
 		return nil, errors.New("Passphrases do not match.")
@@ -45,8 +47,16 @@ func createPassPhrase() ([]byte, error) {
 }
 
 func askPassPhrase(name string) []byte {
-	fmt.Println("Enter passphrase for %v (empty for no passphrase): ", name)
+	fmt.Fprintf(os.Stderr, "Enter passphrase for %v (empty for no passphrase): ", name)
 	pass, _ := terminal.ReadPassword(syscall.Stdin)
-	fmt.Println()
+	fmt.Fprintln(os.Stderr)
 	return pass
+}
+
+func getPassPhrase(c *cli.Context, name string) []byte {
+	if c.IsSet("passphrase") {
+		return []byte(c.String("passphrase"))
+	} else {
+		return askPassPhrase(name)
+	}
 }
